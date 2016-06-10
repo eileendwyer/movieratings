6a87cd8e3a4529d4ca10080a74cad30800d361ec
Eileen Dwyer <eolson@smalltalku.com>
2016-06-10 15:28:21 -0400

    edited models

--- movieratings_app/models.py
+++ movieratings_app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
+
 # Create your models here.
+class Rater(models.Model):
+    rater_id = models.IntegerField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=3, null=True)
+    occupation = models.CharField(max_length=30, null=True)
+    zipcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.rater_id
+
+
+class Movie(models.Model):
+    item_id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    release_date = models.CharField(max_length=30)
+    video_release = models.CharField(max_length=30, default="")
+    imdb_url = models.CharField(max_length=400)
+    unknown = models.IntegerField()
+    action = models.IntegerField()
+    adventure = models.IntegerField()
+    animation = models.IntegerField()
+    children = models.IntegerField()
+    comedy = models.IntegerField()
+    crime = models.IntegerField()
+    documentary = models.IntegerField()
+    drama = models.IntegerField()
+    fantasy = models.IntegerField()
+    film_noir = models.IntegerField()
+    horror = models.IntegerField()
+    musical = models.IntegerField()
+    mystery = models.IntegerField()
+    romance = models.IntegerField()
+    sci_fi = models.IntegerField()
+    thriller = models.IntegerField()
+    war = models.IntegerField()
+    western = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Rating(models.Model):
+    rater_id = models.ForeignKey(Rater)
+    item_id = models.ForeignKey(Movie)
+    rating = models.IntegerField()
+    timestamp = models.IntegerField()
+
+    def __str__(self):
+        return self.item_id
